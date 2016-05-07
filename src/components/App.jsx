d21017966b86eac3ff78e2c3c808b52f2e28fba0
aphavichitr <andrew.phavichitr@gmail.com>
2016-05-06 22:00:29 -0700

    Fixed searchYoutube Method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,48 +11,29 @@ class App extends React.Component {
   }
 
 
-  _fetchYT() {
-    $.ajax({
-      url: 'https://www.googleapis.com/youtube/v3/search?part=snippet',
-      data: {key: window.YOUTUBE_API_KEY, q: 'cats', maxResults: 10},
-      type: 'GET',
-      contentType: 'application/json',
-      success: (data) => {
-        console.log('data: ', data.items);
-
-        console.log('state', this.state.videos);
-        var x = this.state.videos;
-        x = x.concat(data.items);
-        this.setState({videos: x});
-      }
-    });
-  }
-
-  addToState(data) {
-    this.setState({
-      videos: data
-    });
-  }
-
   componentWillMount() {
     //this.setState({videos: window.searchYouTube({key: window.YOUTUBE_API_KEY, query: 'react', max: 10}, () => {})});
     console.log('suh: ', this.props.searchYouTube);
-    //this.props.searchYouTube({key: window.YOUTUBE_API_KEY, query: 'react', max: 10}, this.addToState.bind(this));
-    
-    //this.props.searchYouTube({key: window.YOUTUBE_API_KEY, query: 'react', max: 10}, this.addToState);
-
-    this._fetchYT();
-      // console.log('done');
+    this.props.searchYouTube({key: window.YOUTUBE_API_KEY, q: 'cats', maxResults: 10}, (videos) => {
+      this.setState({
+        videos: videos,
+        current: videos[0] 
+      });
+    });
   }
 
   componentDidMount() {
     this._timer = setInterval(
-      this._fetchYT(),
-      //() => this.props.searchYouTube({key: window.YOUTUBE_API_KEY, query: 'react', max: 10}, this.addToState),
+      //this._fetchYT(),
+      () => this.props.searchYouTube({key: window.YOUTUBE_API_KEY, q: 'cats', maxResults: 10}, (videos) => {
+        this.setState({
+          videos: videos,
+          current: videos[0] 
+        });
+      }),
       5000
     );
-    //console.log('HALOO');
-   // console.log('interval', this.props.searchYouTube);
+
   }
 
   componentWillUnmount() {
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,23 +1,15 @@
 var searchYouTube = (options, callback) => {
   var results = [];
-//console.log('options', options);
   $.ajax({
     url: 'https://www.googleapis.com/youtube/v3/search?part=snippet',
     data: options,
     type: 'GET',
     contentType: 'application/json',
     success: (data) => {
-      results.push(data.items);
-      console.log('data', data.items);
       callback(data.items);
     }
-  }).done(function(vals) {
-    //console.log("vals:", vals);
-    return results.push(vals);
-    //console.log(results);
   });
 
-  
 };
 
 
